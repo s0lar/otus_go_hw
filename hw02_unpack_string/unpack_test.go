3df438d59c342f6531c451c37501740abae79ce5
Pavel Sidorov <pavel.sidorov@gmail.com>
2021-12-01 21:46:54 +0300

    hw02_unpack_string: Home work 02

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,11 +16,8 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: "a1", expected: "a"},
+		{input: "a-1", expected: "a-"},
 	}
 
 	for _, tc := range tests {
