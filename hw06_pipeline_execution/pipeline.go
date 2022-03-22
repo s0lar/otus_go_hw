30541c9d61480ef5c58f80e66b73e8e02daefb67
Pavel Sidorov <pavel.sidorov@gmail.com>
2022-03-22 23:05:34 +0300

    hw06_pipeline_execution: Fix

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,7 +9,7 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	out := in
+	out := withDoneChannel(in, done)
 
 	for _, stage := range stages {
 		out = stage(withDoneChannel(out, done))
