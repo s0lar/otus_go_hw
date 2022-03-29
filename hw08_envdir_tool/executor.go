93032b370244465c67376effe684a23bfbf36c33
Pavel Sidorov <pavel.sidorov@gmail.com>
2022-03-29 20:32:03 +0300

    hw08_envdir_tool: Без тестов

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
+
+	for name, envEl := range env {
+		if true == envEl.NeedRemove {
+			if err := os.Unsetenv(name); err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			if err := os.Setenv(name, envEl.Value); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	command := exec.Command(cmd[0], cmd[1:]...)
+	command.Stdout = os.Stdout
+	if err := command.Run(); err != nil {
+		log.Fatal(err)
+	}
 	return
 }
