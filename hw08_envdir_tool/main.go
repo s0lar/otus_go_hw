00679379f3cade4e71f1f14748c1ee57b4f3c736
Pavel Sidorov <pavel.sidorov@gmail.com>
2022-03-29 20:37:10 +0300

    hw08_envdir_tool: Fix comments

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"log"
 	"os"
 )
 
 func main() {
 	if env, err := ReadDir(os.Args[1]); err != nil {
-		//log.Fatal(err)
+		log.Fatal(err)
 	} else {
 		RunCmd(os.Args[2:], env)
-		//RunCmd(os.Args, env)
 	}
 }
