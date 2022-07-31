9e69230c9f4fe5ed8092511dcfe0026e83053ca6
Pavel Sidorov <pavel.sidorov@gmail.com>
2022-07-31 18:48:08 +0300

    hw11_telnet_client: Telnet client

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,50 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", 10*time.Second, "")
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		log.Fatalln("Не задан хост и порт")
+	}
+
+	addr := strings.Join(flag.Args(), ":")
+	tc := NewTelnetClient(addr, *timeout, os.Stdin, os.Stdout)
+
+	if err := tc.Connect(); err != nil {
+		log.Fatalln(err)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	go func() {
+		if err := tc.Receive(); err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println("...Connection was closed by peer")
+		cancel()
+	}()
+
+	go func() {
+		if err := tc.Send(); err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println("...EOF")
+		cancel()
+	}()
+
+	<-ctx.Done()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,20 +2,44 @@ package main
 
 import (
 	"io"
+	"net"
 	"time"
 )
 
-type TelnetClient interface {
-	Connect() error
-	io.Closer
-	Send() error
-	Receive() error
+type TelnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) *TelnetClient {
+	return &TelnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (t *TelnetClient) Connect() error {
+	conn, err := net.DialTimeout("tcp", t.address, t.timeout)
+	if err != nil {
+		return err
+	}
+
+	t.conn = conn
 	return nil
 }
-
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *TelnetClient) Send() error {
+	_, err := io.Copy(t.conn, t.in)
+	return err
+}
+func (t *TelnetClient) Receive() error {
+	_, err := io.Copy(t.out, t.conn)
+	return err
+}
+func (t *TelnetClient) Close() error {
+	return t.conn.Close()
+}
